544122f4e188caa3fbbc04daa41d947665b01c5f
MishaHnatyshyn <gantyshynmisha@gmail.com>
2020-12-06 16:45:03 +0200

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle-[hash].js'
+    },
+    resolve: {
+        extensions: ['.ts']
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, './dist'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 3000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                options: {
+                    configFile: "tsconfig.json"
+                },
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(svg|woff|woff2|ttf|eot|otf)([\?]?.*)$/,
+                use: {loader: 'file-loader?name=assets/fonts/[name].[ext]'},
+            }
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style-[hash].css',
+        }),
+    ]
+};
